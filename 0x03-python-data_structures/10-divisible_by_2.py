ad5e07dd49fcb60c4966f4fca9aa3210fff0094a
Emmie05 <emmvicadforster@gmail.com>
2023-08-14 17:51:15 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -8,19 +8,4 @@ def divisible_by_2(my_list=[]):
         else:
             multiples.append(False)
 
-#!/usr/bin/python3
-
-
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return(sub(a, b))
     return (multiples)
