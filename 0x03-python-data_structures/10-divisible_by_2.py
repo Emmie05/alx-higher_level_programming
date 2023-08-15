6a6eaf7882f03db4f14ecc1df38596858fca87fd
Emmie05 <emmvicadforster@gmail.com>
2023-08-14 17:43:14 -0700

    Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list."""
+    multiples = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multiples.append(True)
+        else:
+            multiples.append(False)
+
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+
+    else:
+        return(sub(a, b))
+    return (multiples)
