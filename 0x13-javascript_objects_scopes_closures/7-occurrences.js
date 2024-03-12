6da043a09e3fb60da4eb600c0ccb1c0f2a3f606d
Emmie05 <emmvicadforster@gmail.com>
2024-03-12 14:49:59 -0700

    JAVASCRIPT_OBJECTS_SCOPES_CLOSURES

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-
-exports.converter = function (base) {
-  return function (num) {
-    return num.toString(base);
-  };
+exports.nbOccurences = function (list, searchElement) {
+  let nOccurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurrences++;
+    }
+  }
+  return nOccurrences;
 };
